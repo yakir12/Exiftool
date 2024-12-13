5818992cb83b19964832d6eb27e5c6e1e3412aaf
Yakir Gagnon <12.yakir@gmail.com>
2024-12-13 14:43:49 +0100

    add tests

--- src/Exiftool.jl
+++ src/Exiftool.jl
@@ -3,6 +3,6 @@ module Exiftool
 using Perl_jll, Artifacts
 
 exiftool = artifact"exiftool/Image-ExifTool-13.07/exiftool"
-fun(file) = read(`$(perl()) $exiftool $file`, String)
+fun(file; args = "") = read(`$(perl()) $exiftool $args $file`, String)
 
 end # module Exiftool
---
+++ test/runtests.jl
@@ -0,0 +1,15 @@
+using Test
+using Dates
+using Exiftool
+
+function get_recording_datetime(file)
+    txts = strip.(split(Exiftool.fun(file, args = `-T -AllDates -n`), '\t'))
+    dts = [DateTime(txt[1:19], DateFormat("yyyy:mm:dd HH:MM:SS")) for txt in txts if length(txt) > 18]
+    if isempty(dts)
+        return missing
+    else
+        minimum(dts)
+    end
+end
+file = "a.png"
+@test get_recording_datetime(file) == DateTime(2024, 12, 13, 9, 38, 5)
